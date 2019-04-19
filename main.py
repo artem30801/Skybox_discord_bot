35c27f9d1b06166d5e80338c4aaf127d586a5814
Pechenux <belokoniy@gmail.com>
2019-04-19 20:11:27 +0500

    First bugfix. There was a mistake with sort of pages

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ if not os.path.exists(os.path.abspath('pages/')):
 if not os.path.exists(os.path.abspath('frames/')):
     os.mkdir(os.path.abspath('frames/'))
 
-for i in range(2, 301):
+now_pages = 300
+
+for i in range(2, now_pages + 1):
     url = 'http://skyboxcomic.com/'
     url_comics = 'http://skyboxcomic.com/comics/'
 
-    url_r = requests.get(url_comics + str(i))
-    bs = BeautifulSoup(url_r.content)
-
     path = os.path.abspath('pages/' + str(i) + '.jpg')
 
-    print('Viewing:', i, url + bs.find_all('img', {'id': 'comicimage'})[0]['src'])  # human readable
+    print('Viewing:', i)
 
     if not os.path.exists(path):  # don't download if we have already page
+        url_r = requests.get(url_comics + str(i))
+        bs = BeautifulSoup(url_r.content)
         print('Downloading:', i, url + bs.find_all('img', {'id': 'comicimage'})[0]['src'])  # human readable
         r = requests.get(url + bs.find_all('img', {'id': 'comicimage'})[0]['src'], stream=True)  # get file stream
         with open(path, 'wb') as f:  # copy paste from stackoverflow, download file
@@ -32,15 +33,16 @@ for i in range(2, 301):
 
 num = 0
 
-for image in os.listdir(os.path.abspath('pages/')):
-    print('Editing:', image)
+for j in range(2, now_pages + 1):
+    image = '{}.jpg'.format(j)
     im = Image.open(os.path.abspath('pages/' + image))
     count = im.size[1] // 338
 
     # frames = []  # for gifs
 
+    print('Working page:', image, 'Frames done:', num)
+
     for i in range(count):
-        print('Working with:', i, ' frame in page:', image)
         frame_path = os.path.abspath('frames/' + str(num) + '.jpg')
         if not os.path.exists(frame_path):
             box = (0, i * 338, im.size[0], (i + 1) * 338)
